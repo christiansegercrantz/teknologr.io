8c90d0dce74e3b09266604fd1e4240e2c557c318
Thomas L <tlangens@gmail.com>
2018-02-15 22:31:10 +0200

    Base for home view

--- teknologr/katalogen/views.py
+++ teknologr/katalogen/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from members.models import Member
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    context = {}
+    context['persons'] = Member.objects.all() # TODO: not all!
+    context['abc'] = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
+    return render(request, 'browse.html', context)
