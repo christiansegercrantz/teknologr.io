b0cbc68eabecba5578af8c60706d5bc1fe253217
Thomas L <tlangens@gmail.com>
2018-02-18 11:36:16 +0200

    Add profile view base

--- teknologr/katalogen/urls.py
+++ teknologr/katalogen/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^$', 'katalogen.views.home'),
     url(r'^search$', 'katalogen.views.search'),
-    url(r'^person$', 'katalogen.views.profile'),
+    url(r'^([A-ZÅÄÖ])$', 'katalogen.views.startswith'),
+    url(r'^person/(\d)$', 'katalogen.views.profile'),
 ]
--- teknologr/katalogen/views.py
+++ teknologr/katalogen/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from members.models import Member
 from django.db.models import Q
 from functools import reduce
 from operator import and_
 
 
-# Create your views here.
+def _get_base_context():
+    return {'abc': "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ" }
+
 
 def home(request):
-    context = {}
-    context['abc'] = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
+    context = _get_base_context()
     context['persons'] = Member.objects.all() # TODO: not all!
     return render(request, 'browse.html', context)
 
+
 def search(request):
-    context = {}
-    context['abc'] = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
+    context = _get_base_context()
     query = request.GET.get('q')
     query_list = query.split()
     result = Member.objects.all() # TODO: not all!
@@ -28,10 +29,15 @@ def search(request):
 
     return render(request, 'browse.html', context)
 
-def profile(request):
-    # TODO: implement
-    context = {}
-    context['abc'] = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
-    context['persons'] = Member.objects.all() # TODO: not all!
-    return render(request, 'browse.html', context)
 
+def profile(request, member_id):
+    context = _get_base_context()
+    # TODO: implement
+    context['person'] = get_object_or_404(Member, id=member_id)
+    return render(request, 'profile.html', context)
+
+
+def startswith(request, letter):
+    context = _get_base_context()
+    context['persons'] = Member.objects.filter(surname__istartswith=letter) # TODO: not all members!
+    return render(request, 'browse.html', context)
