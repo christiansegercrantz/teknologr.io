6ef6ca3d0aee08750c8a3199b73fc7ef9c48bc68
Thomas Langenskiöld <thomas.langenskiold@vtt.fi>
2018-03-01 16:23:30 +0200

    fix url endpoint to accept multi digit numbers

--- teknologr/katalogen/urls.py
+++ teknologr/katalogen/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$', views.home, name='katalogen.views.home'),
     url(r'^search$', views.search, name='katalogen.views.search'),
     url(r'^([A-ZÅÄÖ])$', views.startswith, name='katalogen.views.startswith'),
-    url(r'^person/(\d)$', views.profile, name='katalogen.views.profile'),
+    url(r'^person/(\d+)$', views.profile, name='katalogen.views.profile'),
     url(r'^profile$', views.myprofile, name='katalogen.views.myprofile'),
 ]
