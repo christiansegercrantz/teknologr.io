9854c1102c340f650188b3ed0af2992f7bb4cfe3
Walliski <walliski@gmail.com>
2018-03-21 11:42:44 +0200

    Show membership type in Katalogen

--- teknologr/members/models.py
+++ teknologr/members/models.py
@@ -60,9 +60,18 @@ class Member(SuperClass):
         first_name = self.preferred_name if self.preferred_name != "UNKNOWN" else self.given_names.split()[0]
         return "%s %s" % (first_name, self.surname)
 
+    def _get_current_member_type(self):
+        member_type = self.getMostRecentMemberType()
+
+        if member_type:
+            return member_type.get_type_display()
+        else:
+            return None
+
     full_name = property(_get_full_name)
     name = property(_get_full_name)
     full_preferred_name = property(_get_full_preferred_name)
+    current_member_type = property(_get_current_member_type)
 
     def __str__(self):
         return self.full_name
@@ -98,7 +107,6 @@ class Member(SuperClass):
             raise error
 
     def getMostRecentMemberType(self):
-
         types = MemberType.objects.filter(member=self).order_by()
 
         if (len(types)) == 0:
