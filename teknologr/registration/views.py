30cc2a07f6e0767d1b084b3588976c6b2509dd26
c00k133 <axel.neergaard@aalto.fi>
2019-06-01 00:39:30 +0300

    Rewrite views as classes

--- teknologr/registration/views.py
+++ teknologr/registration/views.py
@@ -1,28 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.http import HttpResponseRedirect
+from django.views import View
 from members.programmes import DEGREE_PROGRAMME_CHOICES
 from registration.forms import RegistrationForm
+from registration.models import LimboMember
 
 
-def _get_base_context():
-    return {'DEBUG': settings.DEBUG}
+class BaseView(View):
+    context = {'DEBUG': settings.DEBUG}
 
 
-def home(request):
-    context = _get_base_context()
+class HomeView(BaseView):
+    template = 'registration.html'
 
-    if request.method == 'POST':
+    def get(self, request):
+        self.context['programmes'] = DEGREE_PROGRAMME_CHOICES
+        self.context['form'] = RegistrationForm()
+        return render(request, self.template, self.context)
+
+
+class SubmitView(BaseView):
+    template = 'submit.view'
+
+    """
+    def get(self, request):
+        applicant = LimboMember()
+    """
+
+    def post(self, request):
+        # TODO: add context
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/registration/')
-    else:
-        context['programmes'] = DEGREE_PROGRAMME_CHOICES
-        context['form'] = RegistrationForm()
+            registration = form.instance
+            registration.save()
+        else:
+            self.context['form'] = form
+            return render(request, 'registration.html', self.context, status=400)
+            #return redirect('/registration/')
 
-    return render(request, 'registration.html', context)
-
-
-def submit(request):
-    print(request)
-    pass
+        render(request, 'submit.html')
