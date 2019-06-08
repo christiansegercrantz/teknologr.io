8df738f7a9aee542c79fc2873bf13c75c892d398
Axel Neergaard <axel.neergaard@aalto.fi>
2019-06-08 21:44:56 +0300

    Remove registration url comments -> urls ready

--- teknologr/registration/urls.py
+++ teknologr/registration/urls.py
@@ -4,6 +4,4 @@ from registration.views import *
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='registration.views.home'),
     url(r'^submit/$', SubmitView.as_view(), name='registration.views.submit'),
-    # TODO: done url?
-    # TODO: error url?
 ]
