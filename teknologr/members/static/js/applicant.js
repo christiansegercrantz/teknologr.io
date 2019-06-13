bb9a564348fd4592c2422230a613ef34c5124f37
Axel Neergaard <axel.neergaard@aalto.fi>
2019-06-13 23:56:48 +0300

    Remove unnecessary console.log()

--- teknologr/members/static/js/applicant.js
+++ teknologr/members/static/js/applicant.js
@@ -17,7 +17,6 @@ $(document).ready(function() {
     setProgrammeChoice();
 
     $('#id_degree_programme_options').change(function() {
-        console.log('hello');
         if (this.value === 'extra') {
             $('#unknown_degree').show();
             $('#id_degree_programme').val('');
