e2bed8882040cbb2967f6be3a85cf0fe2915ef42
Axel Neergaard <axel.neergaard@aalto.fi>
2019-06-13 23:56:27 +0300

    Set degree choice according to data

--- teknologr/members/static/js/applicant.js
+++ teknologr/members/static/js/applicant.js
@@ -1,4 +1,20 @@
+function setProgrammeChoice() {
+    const programmeOptionsSelector = $('#id_degree_programme_options option');
+    const programmeOptions = Object.values(programmeOptionsSelector).map(option => option.value);
+
+    const currentProgramme = $('#id_degree_programme').val();
+    if (programmeOptions.indexOf(currentProgramme) !== -1) {
+        $('#id_degree_programme_options').val(currentProgramme);
+    } else {
+        $('#id_degree_programme_options').val('extra');
+        $('#unknown_degree').show();
+    }
+}
+
 $(document).ready(function() {
+
+    // Set degree choice based on pre-existing data
+    setProgrammeChoice();
 
     $('#id_degree_programme_options').change(function() {
         console.log('hello');
