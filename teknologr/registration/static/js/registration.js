131204a1518a0af22cce24bb95ab2d410e2c125f
Axel Neergaard <axel.neergaard@aalto.fi>
2019-06-16 00:33:24 +0300

    Put readonly on mother tongue instead of disabled

--- teknologr/registration/static/js/registration.js
+++ teknologr/registration/static/js/registration.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    // There is no default option for disabling a field with django-bootstrap4
-    $('#id_mother_tongue').prop('disabled', true);
+    // There is no default option for setting readonly a field with django-bootstrap4
+    $('#id_mother_tongue').prop('readonly', true);
 
     // Set tooltips
     $('[data-toggle="tooltip"]').tooltip();
@@ -19,10 +19,10 @@ $('#id_degree_programme_options').change(function() {
 $('input:radio[name="language"]').change(function() {
     if ($(this).is(':checked')) {
         if ($(this).val() == 'extra') {
-            $('#id_mother_tongue').prop('disabled', false);
+            $('#id_mother_tongue').prop('readonly', false);
             $('#id_mother_tongue').val('');
         } else {
-            $('#id_mother_tongue').prop('disabled', true);
+            $('#id_mother_tongue').prop('readonly', true);
             $('#id_mother_tongue').val(this.value);
         }
     }
