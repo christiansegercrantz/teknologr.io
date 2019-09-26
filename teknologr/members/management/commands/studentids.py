4ec4d7e3225e1d406193380fd014b81fab8f51c2
Axel Neergaard <axel.neergaard@aalto.fi>
2019-09-26 22:54:26 +0300

    Add get student ids command

---
+++ teknologr/members/management/commands/studentids.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
+from members.models import Member
+from os.path import isfile
+import csv
+
+
+class Command(BaseCommand):
+    help = 'Get a list of student ids based on input file of emails'
+
+    def add_arguments(self, parser):
+        parser.add_argument('email_file', nargs=1, type=str)
+        parser.add_argument('--output-file', nargs='?', type=str, help='Name output file', default='found_ids.csv')
+
+    def handle(self, *args, **options):
+        email_file = options['email_file'][0]
+        if not isfile(email_file):
+            raise CommandError('Could not find input file: {}'.format(email_file))
+
+        with open(email_file, 'r') as fp:
+            reader = csv.reader(fp)
+            emails = [email[0].lower() for email in reader]
+
+        student_ids = [
+            member.student_id
+            for member in filter(
+                lambda member: member.email.lower() in emails,
+                Member.objects.all()
+            )
+        ]
+
+        output_file = options['output_file']
+        with open(output_file, 'w+') as fp:
+            fp.write('\n'.join(student_ids))
+
+        self.stdout.write(self.style.SUCCESS('Saved all found student ids to {}'.format(output_file)))
