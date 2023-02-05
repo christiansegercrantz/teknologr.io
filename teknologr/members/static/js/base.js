a08245eefb2065316eb2fc852ce9d128dbe08e14
Filip Stenbacka <filip.stenbacka@gmail.com>
2023-02-05 22:10:57 +0200

    Add a helper method that adds creates a listener that does a request, since the js files in this project is filled with very similar looking code.

--- teknologr/members/static/js/base.js
+++ teknologr/members/static/js/base.js
@@ -1,6 +1,59 @@
 // Sidebar ajax search delay timer
 var timer;
 
+/**
+ * Helper method for calling a function, if it is a function.
+ */
+const call_if_function = (fn, ...params) => {
+	return typeof fn === "function" ? fn(...params) : fn;
+}
+
+/**
+ * Helper function for adding a listener to an element that does a request.
+ *
+ * @param {Object} options
+ * @param {String} option.element          The element(s) to add the listener to
+ * @param {String} option.method           The request method
+ * @param {String} option.url              The request url
+ * @param {Object} [option.data]           The request data (default: Element.serialize())
+ * @param {String} [option.confirmMessage] An optional confirm message
+ * @param {String} [option.newLocation]    The url to redirect to after the request is done (default: reload current page)
+ *
+ * Some of the options also allow a function (e: Element) => T to be passed, if the option value depends on for example data on the specific element. These options are:
+ * @param {String | (e: Element) => String} option.url
+ * @param {Object | (e: Element) => Object} dption.data
+ * @param {String | (e: Element) => String} option.newLocation
+ */
+const add_request_listener = options => {
+	const { element, method, url, data, confirmMessage, newLocation } = options;
+	const ELEMENT = $(element);
+
+	let TYPE = ELEMENT.get(0)?.tagName.toLowerCase() === "form" ? "submit" : "click";
+
+	// Add a listener to the element(s)
+	ELEMENT.on(TYPE, event => {
+		event.preventDefault();
+
+		if (confirmMessage && !confirm(confirmMessage)) return;
+
+		// Do the request
+		const request = $.ajax({
+			method,
+			url: call_if_function(url, ELEMENT),
+			data: data ? call_if_function(data, ELEMENT) : ELEMENT.serialize(),
+		});
+
+		// Add listeners to the request
+		request.done(msg => {
+			if (newLocation) window.location = call_if_function(newLocation, ELEMENT, msg);
+			else location.reload();
+		});
+		request.fail((jqHXR, textStatus) => {
+			alert(`Request failed (${textStatus}): ${jqHXR.responseText}`);
+		});
+	});
+}
+
 $(document).ready(function() {
 	$("#newform").submit(function(event){
 		var active = $(this).data('active');
