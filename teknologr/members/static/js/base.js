4a3fb4cb6152209d4cac9949c4a80303c6086067
Filip Stenbacka <filip.stenbacka@gmail.com>
2023-05-18 19:40:47 +0300

    The admin page search for functionaries/groups/decorations now also searches in the comments for each item, which helps if the real or alternative name is written as a comment while for example an acronym is used as the actual name. This implementation can become a bit slow if there are many long comments that has to be searched through, but it all happens client side. If snappier search is needed, then I would suggest adding a separate 'alternative names' column into the models. This resolves issue #132.

--- teknologr/members/static/js/base.js
+++ teknologr/members/static/js/base.js
@@ -159,6 +159,7 @@ $(document).ready(function () {
 		var active = $(this).data('active');
 		var filter = $(this).val().toLowerCase();
 
+		// If members, search with ajax_select Django app by doing a GET request
 		if (active === 'members') {
 			timer && clearTimeout(timer);
 			timer = setTimeout(function(){
@@ -175,13 +176,12 @@ $(document).ready(function () {
 				});
 			}, 300);
 
+		// If not members, simply hide/unhide the elements
 		} else {
-			$("#side-objects a").each(function(index){
-				if($(this).text().toLowerCase().indexOf(filter) > -1) {
-					$(this).css('display', '');
-				} else {
-					$(this).css('display', 'none');
-				}
+			$("#side-objects a").each(function () {
+				const element = $(this);
+				const show = element.text().toLowerCase().indexOf(filter) > -1 || element.attr("search").toLowerCase().indexOf(filter) > -1;
+				element.css("display", show ? "" : "none");
 			});
 		}
 	});
