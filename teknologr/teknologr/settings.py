353b281f04af2f8d0016140b222f1c9d141ac09a
Filip Stenbacka <filip.stenbacka@gmail.com>
2023-05-22 03:17:48 +0300

    Added out-commented DB query debug logging setup.

--- teknologr/teknologr/settings.py
+++ teknologr/teknologr/settings.py
@@ -111,6 +111,25 @@ if not DEBUG:
             },
         },
     }
+'''
+else:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+            }
+        },
+        'loggers': {
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+            },
+        },
+    }
+'''
 
 
 # Database
