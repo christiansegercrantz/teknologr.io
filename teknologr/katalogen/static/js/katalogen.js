2f3a39c2c7be2d1a9b1983510ae43962227682ca
Filip Stenbacka <filip.stenbacka@gmail.com>
2023-06-02 01:28:13 +0300

    Simplified ordering via JS since dates can also be compared as strings.

--- teknologr/katalogen/static/js/katalogen.js
+++ teknologr/katalogen/static/js/katalogen.js
@@ -1,46 +1,22 @@
 /**
  * Order the rows of a table according to a specific column.
  */
-const sortTable = ({ tbody, column, reverse, attributes, dates }) => {
+const sortTable = ({ tbody, column, reverse, from_attribute }) => {
     const selector = `td:eq(${column})`;
 
-    // The data to order can be gotten from either the cell text itself or from the 'order-data-1' and 'order-data-2' attributes, since ordering by the text is not always suitable. For example names might want to be ordered by surname but written as "<first_names> <surname>".
-    let getFirstData, getSecondData;
-    if (attributes) {
-        getFirstData = a => $(selector, a).attr("order-data-1") || "";
-        getSecondData = a => $(selector, a).attr("order-data-2") || "";
+    // The data to order can be gotten from either the cell text itself or from the 'order-data' attribute, since ordering by the text is not always suitable. For example names might want to be ordered by surname but displayed as "<first_names> <surname>", and dates might be displayed in a certain format but must (and can) be compared using the YYYY-MM-DD format.
+    let getData;
+    if (from_attribute) {
+        getData = a => $(selector, a).attr("order-data") || "";
     } else {
-        getFirstData = a =>  $(selector, a).text();
-        getSecondData = () => "";
+        getData = a =>  $(selector, a).text();
     }
+
+    // localeCompare() can compare numbers as strings when the 'numeric' option is set.
+    // XXX: The locale is hardcoded to 'sv' here, but it should probably be handled in some other way
+    const compareFn = (a, b) => getData(b).localeCompare(getData(a), "sv", { usage: "sort", sensitivity: "base", numeric: true });
 
     let rows = tbody.find("tr");
-    let compareFn;
-    if (dates) {
-        compareFn = (a, b) => {
-            const a1 = new Date(getFirstData(a));
-            const b1 = new Date(getFirstData(b));
-            if (b1 > a1) return 1;
-            if (a1 > b1) return -1;
-
-            const a2 = new Date(getSecondData(a));
-            const b2 = new Date(getSecondData(b));
-            if (b2 > a2) return 1;
-            if (a2 > b2) return -1;
-            return 0;
-        }
-    } else {
-        // localeCompare() can also compare numbers as strings when the 'numeric' option is set
-        const options = { usage: "sort", sensitivity: "base", numeric: true };
-
-        // XXX: The locale is hardcoded to 'sv' here, but it should probably be handled in some other way
-        compareFn = (a, b) => {
-            const result = getFirstData(b).localeCompare(getFirstData(a), "sv", options);
-            if (result !== 0) return result;
-            return getSecondData(b).localeCompare(getSecondData(a), "sv", options);
-        }
-    }
-
     if (reverse) {
         rows = rows.sort((a, b) => compareFn(a, b));
     } else {
@@ -68,8 +44,7 @@ $(document).ready(() => {
                 tbody,
                 column,
                 reverse,
-                attributes: $(e).hasClass("attributes"),
-                dates: $(e).hasClass("dates"),
+                from_attribute: $(e).hasClass("attribute"),
             });
             tbody.attr("order-by", column);
             tbody.attr("order-reverse", reverse ? "1" : "");
