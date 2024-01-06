a49fda9d6ec51bdd90b569ee201611464dbfe1d8
Filip Stenbacka <filip.stenbacka@gmail.com>
2024-01-06 03:35:58 +0200

    Fixed failing test.

--- teknologr/members/tests_models.py
+++ teknologr/members/tests_models.py
@@ -217,19 +217,6 @@ class MemberTest(TestCase):
         self.assertEquals('StÄlM', member.current_member_type)
         self.assertFalse(member.shouldBeStalm())
 
-    def test_saving(self):
-        member = Member(
-            given_names='Svatta',
-            surname='Teknolog',
-        )
-        member.save()
-
-        # try to cover lines missed by conditions.
-        member.username = 'teknosv1'
-        member.student_id = '123456'
-        member.email = 'svatta@teknolog.fi'
-        member.save()
-
 
 class MemberOrderTest(BaseTest):
     def test_order_by(self):
